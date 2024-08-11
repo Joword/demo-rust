c90046ac05ed05236a1195918d0faf4615035c84
joword <23089538@qq.com>
2024-08-11 22:33:47 +0800

    update the models and alg

--- commons/shazam/models.rs
+++ commons/shazam/models.rs
@@ -1,13 +1,13 @@
 
-trait Name {
-    song_id: u32
-    anchor_time: u32
+pub struct Name {
+    song_id: u32,
+    anchor_time: u32,
 }
 
-trait RecodeData {
-    audio: &str
-    duration: f64
-    channels: i32
-    sample_rate: i32
-    sample_size: i32
+pub struct RecodeData {
+    audio: &str,
+    duration: f64,
+    channels: i32,
+    sample_rate: i32,
+    sample_size: i32,
 }
